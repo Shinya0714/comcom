78915bc6f725b63dfce18c4f1d4cdeaf40eebedb
Shinya0714 <doradoranobinobi93871293@gmail.com>
2024-09-16 10:18:53 +0900

    feat:環境構築

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module amen_hallelujah_peanutButter/comcom/backend
+
+go 1.22.4
---
+++ frontend/app/page.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span className="ml-3 text-xl">comcom</span>
+          </a>
+        </div>
+      </header>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
+            <form>
+              <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                  <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
+                      <label htmlFor="editor" className="sr-only">Publish post</label>
+                      <textarea id="editor" className="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a something..." required ></textarea>
+                  </div>
+              </div>
+              <button type="submit" className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                  Send
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
